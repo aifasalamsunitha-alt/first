6b896fee916fa1f7def3561cea9cfd8c4a7bdeac
aifasalamsunitha-alt <aifasalamsunitha@gmail.com>
2026-06-11 13:49:40 +0530

    second commit added python file

---
+++ matchdemo.py
@@ -0,0 +1,10 @@
+# num=5
+# match num:
+#     case 1:
+#         print("one")
+#     case 2:
+#         print("two")
+#     case 3:
+#         print("three")
+#     case _:
+#         print("incorrect number")
