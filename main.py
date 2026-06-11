50e3ebdf8ed948e13989478803d0278705d12bad
aifasalamsunitha-alt <aifasalamsunitha@gmail.com>
2026-06-11 15:09:09 +0530

    1-11

---
+++ main.py
@@ -0,0 +1,103 @@
+# This program calculates the average and percentage of three numbers.
+# num1 =55
+# num2= 66
+# num3= 77
+# # Calculate the average
+# average = (num1 + num2 + num3) / 300
+# # Calculate the percentage
+# total = num1 + num2 + num3
+# percentage = (total / 300) * 100
+# # Print the results
+# print("Average:", average)
+# print("Percentage:", percentage)
+
+# x=input()
+# print(x)
+# print(type(x))
+
+# x=float(input("enter the price:"))
+# print(x)
+# print(type(x))
+
+# name = input("Enter your name: ")
+# x=float(input("Enter your mark for Maths: "))
+# y=float(input("Enter your mark for English: "))
+# z=float(input("Enter your mark for Science: "))
+# average = (x + y + z) / 3
+# percentage = (x + y + z) / 300 * 100
+# print("Name:", name)
+# print("Average:", average)
+# print("Percentage:", percentage)
+
+# name = input("Enter your name: ")
+# x=int(input("Enter the age: "))
+# if x >= 18:
+#     print(name, "can vote.")
+# else:
+#     print(name, "cannot vote.")
+
+# num=int(input("Enter a number: "))
+# if num % 2 == 0:
+#     print(num, "is an even number.")
+# else:
+#     print(num, "is an odd number.")
+
+# num1=int(input("Enter the first number: "))
+# num2=int(input("Enter the second number: "))
+# num3=int(input("Enter the third number: "))
+# if num1 > num2 and num1 >num3:
+#     print(num1, "is the largest number.")
+# elif num2 > num1 and num2 > num3:
+#     print(num2, "is the largest number.")
+# elif num3 >num1 and num3 > num2:
+#     print(num3, "is the largest number.") 
+# else:
+#     print("either all numbers are equal or  an error in input.") 
+
+# num=float(input("enter the age:"))
+# if num >=0 and num <= 8:
+#     print("kid")
+# elif num > 8 and num <= 12:
+#     print("junior")
+# elif num > 12 and num <= 18:
+#     print("teenager")
+# elif num > 18 and num <= 50:
+#     print("adult")
+# elif num > 50:
+#     print("senior citizen")
+# else:
+#     print("there is an error.")
+
+# homework
+# num1=int(input("Enter the first number: "))
+# num2=int(input("Enter the second number: "))
+# num3=int(input("Enter the third number: "))
+# num4=int(input("Enter the fourth number: "))
+# if num1 > num2 and num1 >num3 and num1 > num4:
+#     print(num1, "is the largest number.")
+# elif num2 > num1 and num2 > num3 and num2 > num4:
+#     print(num2, "is the largest number.")
+# elif num3 >num1 and num3 > num2 and num3 > num4:
+#     print(num3, "is the largest number.")
+# elif num4 > num1 and num4 > num2 and num4 > num3:
+#     print(num4, "is the largest number.")
+# else:
+#     print("either all numbers are equal or  an error in input.")
+
+
+# name=input("Enter your name: ")
+# x=float(input("Enter your mark for Maths: "))
+# y=float(input("Enter your mark for English: "))
+# z=float(input("Enter your mark for Science: "))
+# average = (x + y + z) / 3
+# percentage = (x + y + z) / 300 * 100
+# print("Name:", name)
+# print("Average:", average)
+# print("Percentage:", percentage)
+# if percentage >= 50:
+#     print("Hi Mr./Miss", name, "Congratulations! You passed.")
+# else:
+#     print("Hi Mr./Miss", name, "Sorry! You failed.")
+    
+for i in range(1,11):
+    print(i)
